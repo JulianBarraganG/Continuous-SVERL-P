4f366f784d1c63cb3c584b69b8dbc25bf4f26fed
JulianBarraganG <xjulianbarragan@gmail.com>
2025-04-22 10:20:46 +0200

    RandomSample now takes all unobserved features from the same data sample

--- src/sverl/RandomSampler.py
+++ src/sverl/RandomSampler.py
@@ -1,10 +1,18 @@
 import numpy as np 
 
 class RandomSampler:
+    """
+    RandomSampler imputes missing features, given some observed features,
+    by randomly selecting values from the same data sample.
+    The class imputes **unconditioned** on the observed features.
+    Args:
+        data: A numpy array of shape (n, d), where n is the number of samples and d is the number of features.
+        null_value: The value used to indicate missing features in the data.
+    """
     def __init__(self, data, null_value=0):
         self.data = data
-        self.size = data.shape[1]
-        self.length = data.shape[0]
+        self.size = data.shape[1] # Number of features d
+        self.length = data.shape[0] # Number of samples n
         self.null_value = null_value
 
 
@@ -19,10 +27,10 @@ class RandomSampler:
 
         pred = np.zeros(self.size)
 
+        # Get missing features from the same data sample
+        idx = np.random.randint(0, self.length)
         for i, b in enumerate(mask): 
             if b == self.null_value: 
-                #sample from the data
-                idx = np.random.randint(0, self.length)
                 pred[i] = self.data[idx, i]
             else:
                 pred[i] = observed_features[i]
