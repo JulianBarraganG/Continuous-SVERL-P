ebd3a463a73394fe40ec5ce9bbfcfb27385a685a
OskarLinke <olinke2@gmail.com>
2025-04-24 13:00:14 +0200

    Fixed the TTT env, so it acts more like a gym env. Implemented a q-learning agent which doesn't learn :(

--- src/sverl/run_ttt.py
+++ src/sverl/run_ttt.py
@@ -1,11 +1,33 @@
-from tictactoe_gym.envs.tictactoe_env import TicTacToeEnv
+from TTTenv import TTT
+import numpy as np
+import shapley
+import utils
+import NeuralConditioner 
+import RandomSampler
+from tqdm import tqdm, trange  
+import ttt_agent
 
-env = TicTacToeEnv()
-env.reset()
-
-env.step(0)
-lst = env.step(7)
+env = TTT()
 
 
-for i, ele in enumerate(lst):
-    print(f"Ele {i}:", ele)
+
+agent = ttt_agent.Agent(env.observation_space.shape[0], env.num_actions, epsilon=0.05, gamma=1, alpha=0.2)
+print("Training agent...")
+
+ttt_agent.train(agent, env, 1e6)
+
+##This is to demonstrate that the agent is not learning.
+
+state, _ = env.reset()
+print(state)
+state,reward, done, _, _ = env.step(agent.choose_action(state))
+print(state, reward, done)
+state,reward, done, _, _ = env.step(agent.choose_action(state))
+print(state, reward, done)
+state,reward, done, _, _ = env.step(agent.choose_action(state))
+print(state, reward, done)
+state,reward, done, _, _ = env.step(agent.choose_action(state))
+print(state, reward, done)
+state,reward, done, _, _ = env.step(agent.choose_action(state))
+print(state, reward, done)
+
