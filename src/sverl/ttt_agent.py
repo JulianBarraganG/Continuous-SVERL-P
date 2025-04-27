fe1816991c93fea038b39ea6d9edb95886ca7576
OskarLinke <olinke2@gmail.com>
2025-04-27 11:56:41 +0200

    Changed the agent, so it works with minmax without having to use the dictionairy generated from the environment. When it encounters an unseen board-position, it can minmax optimal moves. So that's one of the previously mentioned issues solved

--- src/sverl/ttt_agent.py
+++ src/sverl/ttt_agent.py
@@ -1,6 +1,6 @@
 import numpy as np
 from collections import defaultdict
-from ttt_env import TTT, ScoreDict
+from ttt_env import TTT, ScoreDict, WonDict, ValidDict
 from tqdm import trange
 import copy
     
@@ -9,28 +9,65 @@ import copy
 
 class Agent:    
     def __init__(self):
+        self.action_dict = {0: (0, 0),
+                            1: (0, 1),
+                            2: (0, 2),
+                            3: (1, 0),
+                            4: (1, 1),
+                            5: (1, 2),
+                            6: (2, 0),
+                            7: (2, 1),
+                            8: (2, 2)}
+        self.won_dict = WonDict()
+
+        # Save previously calculated valid actions for boards, also speed.
+        self.valid_dict = ValidDict()
+
+        # Save previous scores for minmax, speed.
         self.score_dict = ScoreDict()
+        self.score_dict.won_dict = self.won_dict
+        self.score_dict.valid_dict = self.valid_dict
+        self.score_dict.action_dict = self.action_dict
         self.score_dict.score_dict = self.score_dict
     
-    def train(self, env): 
-        starting_states = [np.zeros(9)]
+    def score(self, state, player):
+        """
+        Given the game state and whose turn it is returns a tuple (estimated game score, best move to play)
+        """
 
-        for i in range(9): 
-            pos = np.zeros(9)
-            pos[i] = 1
-            starting_states.append(pos)
-            pos = np.zeros(9)
-            pos[i] = 2
-            starting_states.append(pos)
-        for state in starting_states: 
-            _, _ = env.reset(start_state = state)
-            env.minmax_player()
+        state_byte = state.tobytes()
 
-        self.score_dict = env.get_optimal_policy_dict()         
+        done, winner = self.won_dict[state_byte]
+
+        if not done: end_score = None
+        else: end_score = (winner + 1) % 3 - 1
+
+        if end_score is not None: return end_score, None
+        else:
+            all_moves = self.valid_dict[state_byte]
+                        
+            scores = np.empty(len(all_moves))
+            
+            n_player = player % 2 + 1
+
+            for i, action in enumerate(all_moves):
+
+                new_state = state.copy()
+                new_state[self.action_dict[action]] = player
+
+                current_score, _ = self.score_dict[tuple([new_state.tobytes(), n_player])]
+
+                scores[i] = current_score
+
+            if player == 1: best_score = max(scores)
+            elif player == 2: best_score = min(scores)
+
+            best_moves = all_moves[scores == best_score]
+                        
+            return best_score, best_moves      
 
     def choose_action(self, state): 
         if(state.shape == (9,)): 
             state = state.reshape(3,3)
-        print(state)
         _, best_moves = self.score_dict[tuple([state.tobytes(), 1])]
         return np.random.choice(best_moves)
