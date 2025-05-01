612e8756c998146dc153d7caab5834b67366f173
JulianBarraganG <xjulianbarragan@gmail.com>
2025-05-01 16:40:21 +0200

    fixed some docstrings and resolved outcmaes/ issue

--- src/sverl/cartpole_agent.py
+++ src/sverl/cartpole_agent.py
@@ -13,53 +13,54 @@ class PolicyCartpole(nn.Module):
 
         Parameters
         ----------
-            state_space_dimension (int): 
-                Dimension of the state space.
-            action_space_dimension (int):
-                Dimension of the action space.
-            num_neurons (int):
-                Number of neurons in the hidden layer.
-            bias (bool):
-                Whether to include bias in the linear layers. Should be False for CartPole.
+        state_space_dimension : int 
+            Dimension of the state space.
+        action_space_dimension : int
+            Dimension of the action space.
+        num_neurons : int
+            Number of neurons in the hidden layer.
+        bias : bool
+            Whether to include bias in the linear layers. Should be False for CartPole.
         """
         super(PolicyCartpole, self).__init__()
         self.state_space_dimension = state_space_dimension
         self.fc = nn.Linear(state_space_dimension, num_neurons, bias=bias)
         self.fc1 = nn.Linear(num_neurons, action_space_dimension, bias=bias)
 
-    def forward(self, x):
+    def forward(self, x: np.ndarray) -> int:
         """
         Forward pass through the network.
         
         Parameters
         ----------
-            x (numpy.ndarray):
-                Input state vector.
+        x : np.ndarray
+            Input state vector.
         Returns
         -------
-            int:
-                Action to be taken (0 or 1).
+        int
+            Action to be taken (0 or 1).
         """
         x = torch.Tensor( x.reshape((1, self.state_space_dimension)) )
         hidden = torch.tanh(self.fc(x))
         output = self.fc1(hidden)
         return int(output>0)
     
-#This just comes from the CMA assignment
-def fitness_cart_pole(x, nn, env):
-    '''
+def fitness_cart_pole(x: np.ndarray, nn: torch.nn.Module, env) -> float:
+    """
     Returns negative accumulated reward for single pole, fully environment.
 
     Parameters
     ----------
-        x : Parameter vector encoding the weights.
-        nn: Parameterized model.
-        env: Environment ('CartPole-v?').
-    '''
+    x : np.ndarray
+        Parameter vector encoding the weights.
+    nn : torch.nn.Module 
+        Parameterized model.
+    env : gym.Env
+        Environment ('CartPole-v?').
+    """
     torch.nn.utils.vector_to_parameters(torch.Tensor(x), nn.parameters())  # Set the policy parameters
     state_space_dimension = env.observation_space.shape[0]  # State space dimension
     state = env.reset()[0]  # Forget about previous episode
-    
           
     R = 0  # Accumulated reward
     while True:
@@ -73,44 +74,39 @@ def fitness_cart_pole(x, nn, env):
             return -R  # Episode ended, we consider minimization
     return -R  # Never reached  
 
-
-
-#Again, just comes from the assignment
 def train_cartpole_agent(policy_net , env, ftarget=-9999.9):  
     """
     Function to train a policy network for the CartPole environment using CMA-ES.   
     
     Parameters
     ----------
-        policy_net (PolicyCartpole): 
-            Policy network to be trained.
-        env (gym.Env): 
-            CartPole environment.
-        ftarget (float): 
-            Target fitness value for CMA-ES. Default is -9999.9.
-    
+    policy_net : PolicyCartpole 
+        Policy network to be trained.
+    env : gym.Env
+        CartPole environment.
+    ftarget : float
+        Target fitness value for CMA-ES. Default is -9999.9.
     Returns
     -------
-        policy_net (PolicyCartpole): 
-            Trained policy network.
+    policy_net : PolicyCartpole
+        Trained policy network.
     """   
+    # Set the random seed for reproducibility
     d = sum(param.numel() for param in policy_net.parameters())
     initial_weights = np.random.normal(0, 0.01, d)  # Random parameters for initial policy, d denotes the number of weights
     initial_sigma = .01 # Initial global step-size sigma
+
     # Do the optimization
+    cma_options = {'ftarget': ftarget, 'tolflatfitness':1000, 'eval_final_mean':False, 'verb_filenameprefix': '', 'verb_log': 0}
     res = cma.fmin(fitness_cart_pole,  # Objective function
                 initial_weights,  # Initial search point
                 initial_sigma,  # Initial global step-size sigma
                 args=([policy_net, env]),  # Arguments passed to the fitness function
-                options={'ftarget': ftarget, 'tolflatfitness':1000, 'eval_final_mean':False})
+                   options=cma_options)
     env.close()
   
     # Set the policy parameters to the final solution
     torch.nn.utils.vector_to_parameters(torch.Tensor(res[0]), policy_net.parameters())      
 
     return policy_net  # Return the policy network
-
-
-
-
 
