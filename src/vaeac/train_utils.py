bd7074fc41095406959b88dcc0541734f40a09e4
JulianBarraganG <xjulianbarragan@gmail.com>
2025-05-04 10:33:20 +0200

    cleaned up a bit in vaeac, might be more to be done

--- src/vaeac/train_utils.py
+++ src/vaeac/train_utils.py
@@ -1,6 +1,16 @@
-import torch
+from copy import deepcopy
 from tqdm import tqdm
+from sys import stderr
+
 from dataclasses import dataclass
+from math import ceil
+import numpy as np
+import torch
+from torch.utils.data import DataLoader
+
+from .datasets import compute_normalization
+from .imputation_networks import get_imputation_networks
+from .VAEAC import VAEAC
 
 @dataclass
 class TrainingArgs:
@@ -72,3 +82,161 @@ def get_validation_iwae(val_dataloader, mask_generator, batch_size,
         if verbose:
             iterator.set_description('Validation IWAE: %g' % avg_iwae)
     return float(avg_iwae)
+
+def get_vaeac(args: TrainingArgs, one_hot_max_sizes: list, data: str | np.ndarray):
+    """
+    given a specification on state feature data types, and trajectory data file path,
+    this function will train a vaeac model on the data, and return the vaeac model.
+    parameters
+    ----------
+    args : TrainingArgs
+    one_hot_max_sizes : list
+    data : str | np.ndarray
+    returns
+    -------
+    vaeac : vaeac
+        the trained vaeac model as a pytorch module.
+    """
+    # read and normalize input data
+    if isinstance(data, str):
+        try:
+            raw_data = np.loadtxt(data, delimiter=',')
+        except FileNotFoundError:
+            raise FileNotFoundError(
+                    f"data file not found. please check the path: {data},"
+                    + "and make sure you call from src directory."
+                    )
+    else:
+        raw_data = data
+
+    networks = get_imputation_networks(one_hot_max_sizes)
+
+    args = TrainingArgs()
+
+    raw_data = torch.from_numpy(raw_data).float()
+    norm_mean, norm_std = compute_normalization(raw_data, one_hot_max_sizes)
+    norm_std = torch.max(norm_std, torch.tensor(1e-9))
+    data = (raw_data - norm_mean[None]) / norm_std[None]
+
+    # default parameters which are not supposed to be changed from user interface
+    use_cuda = torch.cuda.is_available()
+    verbose = True
+    # non-zero number of workers cause nasty warnings because of some bug in
+    # multiprocess library. it might be fixed now, but anyway there is no need
+    # to have a lot of workers for dataloader over in-memory tabular data.
+    num_workers = 0
+
+    # design all necessary networks and learning parameters for the dataset
+
+    # build vaeac on top of returned network, optimizer on top of vaeac,
+    # extract optimization parameters and mask generator
+    model = VAEAC(
+        networks['reconstruction_log_prob'],
+        networks['proposal_network'],
+        networks['prior_network'],
+        networks['generative_network'],
+        networks['sampler'], 
+        one_hot_max_sizes
+    )
+    if use_cuda:
+        model = model.cuda()
+    optimizer = networks['optimizer'](model.parameters())
+    batch_size = networks['batch_size']
+    mask_generator = networks['mask_generator']
+    vlb_scale_factor = networks.get('vlb_scale_factor', 1)
+
+    # train-validation split
+    val_size = ceil(len(data) * args.validation_ratio)
+    val_indices = np.random.choice(len(data), val_size, False)
+    val_indices_set = set(val_indices)
+    train_indices = [i for i in range(len(data)) if i not in val_indices_set]
+    train_data = data[train_indices]
+    val_data = data[val_indices]
+
+    # initialize dataloaders
+    dataloader = DataLoader(train_data, batch_size=batch_size, shuffle=True,
+                            num_workers=num_workers, drop_last=False)
+    val_dataloader = DataLoader(val_data, batch_size=batch_size, shuffle=True,
+                                num_workers=num_workers, drop_last=False)
+
+    # number of batches after which it is time to do validation
+    validation_batches = ceil(len(dataloader) / args.validations_per_epoch)
+
+    # a list of validation IWAE estimates
+    validation_iwae = []
+    # a list of running variational lower bounds on the train set
+    train_vlb = []
+    # the length of two lists above is the same because the new
+    # values are inserted into them at the validation checkpoints only
+
+    # best model state according to the validation IWAE
+    best_state = None
+
+    # main train loop
+    for epoch in range(args.epochs):
+
+        iterator = dataloader
+        avg_vlb = 0
+        if verbose:
+            print('Epoch %d...' % (epoch + 1), file=stderr, flush=True)
+            iterator = tqdm(iterator)
+
+        # one epoch
+        for i, batch in enumerate(iterator):
+
+            # the time to do a checkpoint is at start and end of the training
+            # and after processing validation_batches batches
+            if any([
+                        i == 0 and epoch == 0,
+                        i % validation_batches == validation_batches - 1,
+                        i + 1 == len(dataloader)
+                    ]):
+                val_iwae = get_validation_iwae(val_dataloader, mask_generator,
+                                               batch_size, model,
+                                               args.validation_iwae_num_samples,
+                                               verbose)
+                validation_iwae.append(val_iwae)
+                train_vlb.append(avg_vlb)
+
+                # if current model validation IWAE is the best validation IWAE
+                # over the history of training, the current state
+                # is saved to best_state variable
+                if max(validation_iwae[::-1]) <= val_iwae:
+                    best_state = deepcopy({
+                        'epoch': epoch,
+                        'model_state_dict': model.state_dict(),
+                        'optimizer_state_dict': optimizer.state_dict(),
+                        'validation_iwae': validation_iwae,
+                        'train_vlb': train_vlb,
+                    })
+
+                if verbose:
+                    print(file=stderr)
+                    print(file=stderr)
+
+            # if batch size is less than batch_size, extend it with objects
+            # from the beginning of the dataset
+            batch = extend_batch(batch, dataloader, batch_size)
+
+            # generate mask and do an optimizer step over the mask and the batch
+            mask = mask_generator(batch)
+            optimizer.zero_grad()
+            if use_cuda:
+                batch = batch.cuda()
+                mask = mask.cuda()
+            vlb = model.batch_vlb(batch, mask).mean()
+            (-vlb / vlb_scale_factor).backward()
+            optimizer.step()
+
+            # update running variational lower bound average
+            avg_vlb += (float(vlb) - avg_vlb) / (i + 1)
+            if verbose:
+                iterator.set_description('Train VLB: %g' % avg_vlb)
+
+    # if use doesn't set use_last_checkpoint flag,
+    # use the best model according to the validation IWAE
+    if not args.use_last_checkpoint:
+        model.load_state_dict(best_state['model_state_dict'])
+
+    return model
+
